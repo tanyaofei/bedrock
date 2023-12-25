c05f129fffd9d217eb7f084a9bc64eef53aea09a
tanyaofei <tan.yaofei@outlook.com>
2023-12-26 00:36:35 +0800

    优化代码

--- src/main/java/io/github/hello09x/bedrock/config/Config.java
+++ src/main/java/io/github/hello09x/bedrock/config/Config.java
@@ -7,7 +7,10 @@ import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashSet;
 import java.util.Objects;
