3c809b1e4e2ac3c17dcf0d8b766b0a3326ab81f5
tanyaofei <tan.yaofei@outlook.com>
2023-12-29 01:00:40 +0800

    wrap task

--- src/main/java/io/github/hello09x/bedrock/util/Folia.java
+++ src/main/java/io/github/hello09x/bedrock/util/Folia.java
@@ -5,8 +5,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 public class Folia {
     private final static boolean IS_FOLIA = Bukkit.getServer().getName().equals("Folia");
@@ -38,5 +40,79 @@ public class Folia {
             Bukkit.getScheduler().runTaskTimer(plugin, runnable, delay, period);
         }
     }
+
+    public static @Nullable WrappedTask runTaskTimer(
+            @NotNull Plugin plugin,
+            @NotNull Entity entity,
+            @NotNull Consumer<@NotNull WrappedTask> task,
+            int delay,
+            int period
+    ) {
+        if (isFolia()) {
+            var t = entity.getScheduler().runAtFixedRate(plugin, x -> {
+                task.accept(new WrappedScheduleTask(x));
+            }, null, delay, period);
+            if (t == null) {
+                return null;
+            }
+            return new WrappedScheduleTask(t);
+        } else {
+            var t = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    task.accept(new WrappedBukkitTask(this));
+                }
+            };
+            return new WrappedBukkitTask(t);
+        }
+    }
+
+
+    public interface WrappedTask {
+        boolean isCancelled();
+
+        void cancel();
+    }
+
+    public static class WrappedScheduleTask implements WrappedTask {
+
+        @NotNull
+        private final ScheduledTask delegate;
+
+        public WrappedScheduleTask(@NotNull ScheduledTask delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return delegate.isCancelled();
+        }
+
+        @Override
+        public void cancel() {
+            delegate.cancel();
+        }
+    }
+
+    public static class WrappedBukkitTask implements WrappedTask {
+
+        @NotNull
+        private final BukkitRunnable delegate;
+
+        public WrappedBukkitTask(@NotNull BukkitRunnable delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return delegate.isCancelled();
+        }
+
+        @Override
+        public void cancel() {
+            delegate.cancel();
+        }
+    }
+
 
 }
